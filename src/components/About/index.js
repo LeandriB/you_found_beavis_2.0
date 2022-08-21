2fe5cbc4127b0e90315061bf87fd307f1fccb128
Leandri Beavis <leandrikuyk@gmail.com>
2022-08-20 21:17:27 -0400

    created About component and added some styling

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h1>
+                <span className="small">Hello,</span>
+                <span className="ease-right-expand">
+                    <span>I'm <span className="color">Leandri.</span></span>
+                </span>
+            </h1>
+            <p className="about-text">
+                I'm a self-taught Front End Web Developer with a background in Fashion Design.
+                I am passionate about learning and enjoy the challenges that come 
+                with creating new applications.
+            </p> 
+            <p className="about-text">
+                My passion for development comes from my love for art and design. I fell in love with
+                programming through backend architecture and quickly realized that I want to know more about the frontend.
+            </p>
+            <p className="about-text">
+                When I am not improving my projects or solving coding problems, you can find me outdoors, hanging out with my dog, 
+                at the gym or, in the kitchen cooking up a storm. 
+            </p>
+        </div>
+    )
+}
+
+export default About
