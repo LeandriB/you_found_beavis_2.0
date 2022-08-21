fcb3c1cfe0c6da78b3a20914d2035dc9a2cc7f80
Leandri Beavis <leandrikuyk@gmail.com>
2022-08-21 16:45:38 -0400

    refactor image links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ function Footer() {
         <footer>
             <section className="social-container">
                 <a href="https://github.com/LeandriB">
-                    <img src="assets/images/github.png" alt="Github" className="social-icons"/>
+                    <img src="/assets/icons/github.png" alt="Github" className="social-icons"/>
                 </a>
                 <a href="https://www.instagram.com/leandrikuyk/">
-                    <img src="assets/images/instagram.png" alt="Instagram" className="social-icons"/>
+                    <img src="/assets/icons/instagram.png" alt="Instagram" className="social-icons"/>
                 </a>
                 <a href="https://www.linkedin.com/in/leandrib/">
-                    <img src="assets/images/linkedin.png" alt="Linkedin" className="social-icons"/>
+                    <img src="/assets/icons/linkedin.png" alt="Linkedin" className="social-icons"/>
                 </a>
             </section>
         </footer>
